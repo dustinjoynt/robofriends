98515bc5109fd3ce18c6474c013b38b56e7908b5
Dustin Joynt <dustinjoynt@gmail.com>
2019-04-25 10:53:20 -0600

    added scroll component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 class Card extends React.Component{
 	render(){
-		
-		const {name, email, id} = this.props
-		
+		const {name, email, id} = this.props	
 		return(
 			<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 				<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,9 +3,7 @@ import Card from './Card'
 
 class CardList extends React.Component{
 	render(){
-
 		const {robots} = this.props
-
 		const cardComponent = robots.map((user, i) => {
 			return (<Card 
 								key={robots[i].id}  
